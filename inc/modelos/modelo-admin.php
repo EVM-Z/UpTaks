4ee50abea4e0b25e416dcc4fbc62542c5410e7df
EliseoVM <elyzeo.vm@gmail.com>
2019-12-30 16:48:04 -0600

    Creando una consulta a la base de datos y enviando...

--- inc/modelos/modelo-admin.php
+++ inc/modelos/modelo-admin.php
@@ -1,5 +1,49 @@
 <?php
-// Nos permite convertir un arreglo en JSON
-// die es como un echo
-die(json_encode($_POST));
+$accion=$_POST['accion'];
+$password=$_POST['password'];
+$usuario=$_POST['usuario'];
+
+if($accion==='crear'){
+    // Codigo para crear los administradores
+    // Hashear password
+    $opciones=array(
+        'cost'=>12
+    );
+    // password_hash encripta el password en un cadena de 60 caracteres
+    $hash_password=password_hash($password, PASSWORD_BCRYPT, $opciones);
+
+    // Importar la conexion
+    include '../funciones/conexion.php';
+
+    try{
+        // Realizar la consulta a la base de datos
+        $stmt=$conn->prepare("INSERT INTO usuarios (usuario, password) VALUES (?, ?) ");
+        // Vamos a ingresar dos strign
+        $stmt->bind_param('ss', $usuario, $hash_password);
+        $stmt->execute();
+        if($stmt->affected_rows){
+            $respuesta=array(
+                'respuesta'=>'correcto',
+                'id_insertado'=>$stmt->insert_id,
+                'tipo'=>$accion
+            );
+        }
+        // Cerramos el stament
+        $stmt->close();
+        // Cerramos la conexion
+        $conn->close();
+    } catch(Exception $e){
+        // En caso de error, tomar la exception
+        $respuesta=array(
+            'pass'=>$e->getMessage()
+        );
+    }
+
+    echo json_encode($respuesta);
+
+    
+}
+if($accion==='login'){
+    // Escribir codigo para loguee a los administradores
+}
 ?>
