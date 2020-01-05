b12790b679a378f0389cb1f7e6fe3a095173d766
EliseoVM <elyzeo.vm@gmail.com>
2020-01-05 15:34:05 -0600

    Agregando la funcionalidad para crear Nuevos Proyectos

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 eventListeners();
+// Lista de proyectos
+var listaProyectos = document.querySelector('ul#proyectos');
 
 function eventListeners() {
     // Boton para crear proyectos
@@ -8,4 +10,28 @@ function eventListeners() {
 function nuevoProyecto(e) {
     e.preventDefault();
     console.log('Presionaste en nuevo proyecto');
+
+    // Crea un input para el nombre del nuevo proyecto
+    var nuevoProyecto = document.createElement('li');
+    nuevoProyecto.innerHTML = '<input type="text" id="nuevo-proyecto">';
+    listaProyectos.appendChild(nuevoProyecto);
+
+    // Selecionar el ID con el nuevoProyecto
+    var inputNuevoProyecto = document.querySelector('#nuevo-proyecto');
+
+    // Al presionar ENTER crear el proyecto
+    inputNuevoProyecto.addEventListener('keypress', function(e) {
+        // wiich o keycode es el codigo de las teclas
+        var tecla = e.which || e.keyCode;
+
+        if (tecla === 13) {
+            guardarProyectoDB(inputNuevoProyecto.value);
+            // removemos el input creado
+            listaProyectos.removeChild(nuevoProyecto);
+        }
+    });
+}
+
+function guardarProyectoDB(nombreProyecto) {
+    console.log(nombreProyecto);
 }
