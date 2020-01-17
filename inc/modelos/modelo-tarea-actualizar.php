2ee04691497f9b44c8bfa24f605e6a2b7b41e466
EliseoVM <elyzeo.vm@gmail.com>
2020-01-17 13:58:39 -0600

    Guardando el estado de las tareas en...

--- inc/modelos/modelo-tarea-actualizar.php
+++ inc/modelos/modelo-tarea-actualizar.php
@@ -2,10 +2,40 @@
 
 $accion=$_POST['accion'];
 $estado=$_POST['estado'];
+$id_tarea=(int) $_POST['id'];
 
 if ($accion === 'actualizar') {
+    // Importar la conexion
+    include '../funciones/conexion.php';
+
+    try{
+        // Realizar la consulta a la base de datos
+        $stmt=$conn->prepare("UPDATE tareas set estado = ? WHERE id = ?");
+        // Vamos a ingresar dos strign
+        $stmt->bind_param('ii', $estado, $id_tarea);
+        $stmt->execute();
+        if($stmt->affected_rows > 0){
+            $respuesta=array(
+                'respuesta'=>'correcto'
+            );
+        }
+        else{
+            $respuesta=array(
+                'respuesta'=>'error'
+            );
+        }
+        // Cerramos el stament
+        $stmt->close();
+        // Cerramos la conexion
+        $conn->close();
+    } catch(Exception $e){
+        // En caso de error, tomar la exception
+        $respuesta=array(
+            'error'=>$e->getMessage()
+        );
+    }
+    echo json_encode($respuesta);
     
-    echo json_encode($_POST);
 }
 
 ?>
